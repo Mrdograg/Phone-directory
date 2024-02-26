39dd941e8bc69b8072901841ed09d3ac26d4a89a
dogra00amit@gmail.com <dogra00amit@gmail.com>
2024-02-26 18:14:36 +0530

    phone directory

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,10 @@
-import React from "react"
+import React, { useContext } from "react"
+import { SubscriberCountContext } from "./SubscriberCountContext"
 export default function Footer() {
+
+    const count =useContext(SubscriberCountContext);
     return (
-        <h4>Number of Enteries: 2</h4>
+        
+        <h4>Number of Enteries: {count}</h4>
     )
 }
